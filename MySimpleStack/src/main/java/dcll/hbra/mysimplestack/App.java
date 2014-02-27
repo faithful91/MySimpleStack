84a2455a9805b5147a2a3532a1935794f8901e47
faithful91 <bradai.hazem@gmail.com>
2014-02-27 23:19:41 +0100

    hh

---
+++ MySimpleStack/src/main/java/dcll/hbra/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package dcll.hbra.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
